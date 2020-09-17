74571710a43f9e8866af406e30633f1c1ed57900
Anthony Chang <sprezzarete@loki>
2020-09-17 13:43:45 +0700

    Initial commit

--- src/main/java/th/in/codex/demo/blog/BlogApp.java
+++ src/main/java/th/in/codex/demo/blog/BlogApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApp.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BlogApp.class, args);
+  }
 }
